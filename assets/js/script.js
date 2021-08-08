72404b16aed5b8f09d66f2d2cceb387be0773be0
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-08-07 20:57:56 -0500

    fetch working

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// appId = "faa63307397f9a8437455fc4cacc3cd2";
+// var currentCity = "";
+// var lastCity = "";
+
+$(document).ready(function() {
+        $("#form-submit").submit(function(event) {
+            performSearch(event);
+        });
+});
+
+    function performSearch(event) {
+        var request;
+        event.preventDefault();
+
+        request = $.ajax({
+            url:'https://api.openweathermap.org/data/2.5/weather',
+            type: "GET",
+            data: {
+                q: $("#city").val(),
+                appid: 'faa63307397f9a8437455fc4cacc3cd2',
+                units: 'imperial'
+            }
+        });
+
+        request.done(function(response) {
+            formatSearch(response);
+        });
+}
+
+function formatSearch(jsonObject) {
+        var city_name = jsonObject.name;
+        var city_weather = jsonObject.weather[0].main;
+        var city_temp = jsonObject.main.temp;
+        
+
+        $("#city-name").text(city_name);
+        $("#city-weather").text(city_weather);
+        $("#city-temp").text(city_temp+ "℉");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#current-conditions").show();
+$("#fiveDay-forecast").show();
